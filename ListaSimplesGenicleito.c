6edfc358cad3b59b22e0f3cb9c7c177385174477
Genicleito <genicleito@live.com>
2014-04-06 11:49:41 -0300

    Update ListaSimplesGenicleito.c

--- ListaSimplesGenicleito.c
+++ ListaSimplesGenicleito.c
@@ -10,6 +10,7 @@ typedef struct{
 
 void Inicializa(ListaInt *l){
 	l->nelems = 0;
+	printf("\n\n");
 }
 
 _Bool consulta(ListaInt l, int x){
@@ -25,56 +26,65 @@ _Bool consulta(ListaInt l, int x){
 		}
 		return false;
 	}
-	
+
 }
 
 _Bool insere(ListaInt *l, int x){
 	int i=0;
 	for(i=0; (i<l->nelems) && (l->chaves[i] != x);i++);
-	if((i == l->nelems) && (l->nelems == MTAMLISTA))
-		return false;
+	if((i == l->nelems) && (l->nelems == MTAMLISTA -1))
+		printf("A lista ja esta cheia!\n\n");
 	else{
 		l->chaves[i] = x;	
 		l->nelems = l->nelems + 1;
-		printf("\n%d\n%d\n%d", l->chaves[0], l->chaves[1], l->chaves[2]);
+		printf("\n%d\n%d\n%d\n\n", l->chaves[0], l->chaves[1], l->chaves[2]);
 	}
 }
 
 _Bool retira(ListaInt *l, int x){
-	
-	return true;
+	int i=0;
+	for(i=0; (i<l->nelems) && (l->chaves[i] != x);i++);
+	if((i == l->nelems) && (l->nelems == MTAMLISTA))
+		printf("\n%d nao esta na lista.\n\n", x);
+	else{
+		l->chaves[i] = 0;
+		l->chaves[i] = l->chaves[l->nelems - 1];
+		l->chaves[l->nelems - 1] = 0;
+		l->nelems = l->nelems - 1;
+		printf("\n%d\n%d\n%d\n\n", l->chaves[0], l->chaves[1], l->chaves[2]);
+	}
 }
 
 int main(){
 	ListaInt y;
-	int OP=0;	
-	
-	while(OP < 4){
+	int OP=0;
+
+	while(OP < 5){
 		printf("\t\t-- Lista Encadeada - By Genicleito Goncalves--\n\n");
 		printf("Menu:\n1. Inicializar Lista;");
 		printf("\n2. Consultar na Lista;");
 		printf("\n3. Inserir na Lista;");
 		printf("\n4. Retirar da Lista.");
-		printf("\n\nOpcao Desejada:");
+		printf("\n\nOpcao Desejada: ");
 		scanf("%d", &OP);
 
 			if (OP == 1) {
 				Inicializa(&y); 
 				//break;
 			}
-			
+
 			else if(OP == 2) {
 				int num;
 				printf("\nNumero para consultar na lista: ");
 				scanf("%d", &num);
 				if(consulta(y, num)){
-					printf("%d esta na lista.", num);
+					printf("%d esta na lista.\n\n", num);
 				}else
-					printf("%d nao esta na lista.", num);;
+					printf("%d nao esta na lista.\n\n", num);
 				//break;
 			}
-		
-			
+
+
 			else if(OP == 3){
 				int x;
 				printf("\nNumero para inserir na lista: ");
@@ -87,10 +97,8 @@ int main(){
 				printf("\nNumero para retirar da lista: ");
 				scanf("%d", &x);
 				retira(&y, x);
-				//break;
 			}
 	}
-	
-	
+
 	return 0;
 }
