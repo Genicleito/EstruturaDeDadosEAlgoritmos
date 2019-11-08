8cfa3e575ce582f0bd11433de82d946de9de72c7
Genicleito Gonçalves <Genicleito@users.noreply.github.com>
2019-11-07 22:10:03 -0300

    array that will be indexed
    You can now index any vector.

--- search_tree.py
+++ search_tree.py
@@ -8,22 +8,17 @@ def insert(t, dic):
     Return the dictionary 'dic' with the characters of 't' as tree nodes.
 
     Parameters:
-    t: string term to insert.
+    t: array that will be indexed.
     dic: dictionary in tree format. It may be empty or already filled.
     """
     if dic == {}:
         dic = {t[0]: {}}
     if len(t) == 1:
-        try: # using 'try' the computational cost is lower
-            dic[t][end_of_word] = {}
+        try: 	# using 'try' the computational cost is lower
+            dic[t[0]][end_of_word] = {}
         except:
-            dic[t] = {end_of_word: {}}
+            dic[t[0]] = {end_of_word: {}}
         return dic
-#         if t in dic.keys(): # using 'if' and 'in' the computational cost is higher
-#             dic[t][end_of_word] = {}
-#         else:
-#             dic[t] = {end_of_word: {}}
-#         return dic
     else:
         try:
             dic[t[0]]
@@ -31,10 +26,6 @@ def insert(t, dic):
             dic[t[0]] = {}
         dic[t[0]] = insert(t[1:], dic[t[0]])
         return dic
-#         if t[0] not in dic.keys(): # costly operation if there are too many nodes at the current tree level.
-#             dic[t[0]] = {}
-#         dic[t[0]] = insert(t[1:], dic[t[0]])
-#         return dic
 
 def search(s, dic):
     """
@@ -57,7 +48,6 @@ def search(s, dic):
     except:
       return False
     return True
-    # return True if end_of_word in sub_tree.keys() else False
 
 
 # inserting words in the dictionary
